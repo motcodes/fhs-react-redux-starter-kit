ad3fe94439658bf76274a81dc96b81a084f7b03f
Matthias Oberholzer <moberholzer.mmt-b2020@fh-salzburg.ac.at>
2022-03-23 15:37:47 +0100

    added json server fetch user on context

--- src/context/db.js
+++ src/context/db.js
@@ -1,10 +1,34 @@
-import React, { createContext, useContext, useState } from 'react'
-import { db } from './data'
+import React, { createContext, useContext, useState, useEffect } from 'react'
+// import { db } from './data'
+
+const dbBaseUrl = 'http://localhost:3001'
+
+const useFetch = (url, options) => {
+  const [response, setResponse] = useState(null)
+  useEffect(() => {
+    const fetcher = async () => {
+      const res = await fetch(url, options)
+      const json = await res.json()
+      setResponse(json)
+    }
+    fetcher()
+  }, [])
+  return response
+}
 
 const DbContext = createContext()
 
 export function DbProvider({ children }) {
-  const [dbData, setDbData] = useState(db)
+  const [dbData, setDbData] = useState([])
+
+  const fetchUser = useFetch(dbBaseUrl + '/user')
+  const fetchMoney = useFetch(dbBaseUrl + '/moneyTransaction')
+
+  useEffect(() => {
+    if (fetchUser && fetchMoney) {
+      setDbData({ user: fetchUser, moneyTransaction: fetchMoney })
+    }
+  }, [fetchUser, fetchMoney])
 
   const value = { dbData, setDbData }
   return <DbContext.Provider value={value}>{children}</DbContext.Provider>
